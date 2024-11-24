88729024eb6f64d26d332413645cc36e62315050
subaru-hello <octosubaru0926@gmail.com>
2024-11-24 11:09:34 +0900

    fix: 毎朝7時50に修正

--- py-functions/src/handlers/tokyo/setagaya/setagaya_sougou_scrape.py
+++ py-functions/src/handlers/tokyo/setagaya/setagaya_sougou_scrape.py
@@ -1,9 +1,12 @@
 from firebase_functions import scheduler_fn
 from src.handlers.tokyo.setagaya.scraping import Scraping
 
-@scheduler_fn.on_schedule(schedule="* * * * *")
+EVERY_07_50_AM_SCHEDULE="50 7 * * *"
+
+@scheduler_fn.on_schedule(schedule=EVERY_07_50_AM_SCHEDULE,
+                          timezone=scheduler_fn.Timezone("Asia/Tokyo"))
 def setagaya_sougou_availability(event: scheduler_fn.ScheduledEvent) -> None:
-    "1分ごとに世田谷総合運動場の陸上競技場貸出状況を取得する関数"
+    "毎朝7時50分に世田谷総合運動場の陸上競技場貸出状況を取得する関数"
     url = "https://www.se-sports.or.jp/facility/sougou/"
     title, body = Scraping(url).execute()
     html = f"""
