a2726b0e7c22a708333ad5e5cb284cf9b42154e0
subaru-hello <octosubaru0926@gmail.com>
2024-11-24 11:04:45 +0900

    feat: cron化させた

--- py-functions/src/handlers/tokyo/setagaya/setagaya_sougou_scrape.py
+++ py-functions/src/handlers/tokyo/setagaya/setagaya_sougou_scrape.py
@@ -1,12 +1,13 @@
-from firebase_functions import https_fn
+from firebase_functions import scheduler_fn
 from src.handlers.tokyo.setagaya.scraping import Scraping
-@https_fn.on_request()
-def setagaya_sougou_availability(req: https_fn.Request) -> https_fn.Response:
-    "世田谷総合運動場の陸上競技場貸出状況を取得する関数"
+
+@scheduler_fn.on_schedule(schedule="* * * * *")
+def setagaya_sougou_availability(event: scheduler_fn.ScheduledEvent) -> None:
+    "1分ごとに世田谷総合運動場の陸上競技場貸出状況を取得する関数"
     url = "https://www.se-sports.or.jp/facility/sougou/"
     title, body = Scraping(url).execute()
     html = f"""
     <h1>{title}</h1>
     <div>{body}</div>
     """
-    return https_fn.Response(html, status=200)
+    print(html)
